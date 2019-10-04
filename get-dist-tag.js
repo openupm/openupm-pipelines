30d357b6450401c6eb34dd95a4d37db8e702e425
Favo Yang <favoyang@gmail.com>
2019-10-04 11:25:08 +0800

    ESLint.

--- get-dist-tag.js
+++ get-dist-tag.js
@@ -1,25 +1,27 @@
-// Get dist tag based on local version and latest version
+/* Get npm dist-tag by comparing local version and latest version.
+ *
+ * Npm server set dist-tag to "latest" for the last published package. And prevent
+ * package publish if local version is less than or equal to latest version. To
+ * publish a package at lower version (a patch or mis-ordered publish), a dist-tag is
+ * required.
+ */
+const compareVersions = require("compare-versions");
 
-const compareVersions = require('compare-versions');
-
-// Get dist tag.
-const getDistTag = function (localVer, latestVer) {
+// Get dist tag, if localVer >= latestVer return latest, otherwise patch@localVer.
+const getDistTag = function(localVer, latestVer) {
   try {
-    // If localVer >= latestVer return latest, otherwise patch@localVer.
     let ret = compareVersions(localVer, latestVer);
-    if (ret == 0 || ret == 1)
-      return 'latest';
-    else
-      return `patch@${localVer}`;
+    if (ret == 0 || ret == 1) return "latest";
+    else return `patch@${localVer}`;
   } catch (err) {
     // Not valid semver, always return latest.
-    return 'latest';
+    return "latest";
   }
 };
 
 if (require.main === module) {
   if (process.argv.length < 4) {
-    console.log('Usage: node get-dist-tag.js <local_version> <latest_version>');
+    console.log("Usage: node get-dist-tag.js <local_version> <latest_version>");
     process.exit(1);
   } else {
     console.log(getDistTag(process.argv[2], process.argv[3]));
